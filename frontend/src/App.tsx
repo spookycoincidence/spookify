f64b55408cf86320f96bbaea20137a8bfff193ce
Huilén Vilches <54365898+spookycoincidence@users.noreply.github.com>
2025-04-15 16:08:33 -0300

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Album from './pages/Album';
+import Album from './pages/Album.tsx';
 
 function App() {
   return (
